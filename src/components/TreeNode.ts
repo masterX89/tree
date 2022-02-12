45b8c53ace4d9bedd9dd15807c77bb7d057e5923
masterX89 <xhk1995@gmail.com>
2022-02-12 17:07:55 +0800

    feat(TreeNode): add TreeNode Tmpl

---
+++ src/components/TreeNode.ts
@@ -0,0 +1,10 @@
+import { defineComponent, h, render } from 'vue'
+
+export default defineComponent({
+  name: 'TreeNode',
+  props: {},
+  setup(props) {},
+  render() {
+    return h('li', 'aaa')
+  }
+})
